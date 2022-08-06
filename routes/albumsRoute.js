8940dc603995c551b0dfc5b367de421ea202460c
RicardoRonan <ricardoronanm@gmail.com>
2022-08-06 14:55:57 +0200

    added routes

---
+++ routes/albumsRoute.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+const middleware = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM albums", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Gets one albums
+router.get("/:id", (req, res) => {
+  try {
+    res.send({ id: req.params.id });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+router.post("/", middleware, (req, res) => {
+  console.log(req.user.user_type);
+  if (req.user.user_type === "admin") {
+    const {
+      sku,
+      name,
+      price,
+      weight,
+      descriptions,
+      thumbnail,
+      image,
+      category,
+      stock,
+    } = req.body;
+    let create_date = new Date().toISOString().slice(0, 19).replace("T", " ");
+    try {
+      con.query(
+        `INSERT into albums (sku,name,price,weight,descriptions,thumbnail,image,category,create_date,stock) values ('${sku}', '${name}', '${price}', '${weight}', '${descriptions}' , '${thumbnail}', '${image}' , '   ${category}' , '${create_date}' , '${stock}')`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+// edit product
+router.put("/:id", middleware, (req, res) => {
+  if (req.user.user_type === "admin") {
+    const {
+      sku,
+      name,
+      price,
+      weight,
+      descriptions,
+      thumbnail,
+      image,
+      category,
+      create_date,
+      stock,
+    } = req.body;
+    try {
+      con.query(
+        `UPDATE albums SET sku="${sku}",name="${name}",price="${price}",weight="${weight}}",descriptions="${descriptions}",thumbnail="${thumbnail}",image="${image}",category="${category}",create_date="${create_date},stock="${stock}" WHERE product_id="${req.params.id}"`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+// delete albums
+router.delete("/:id", middleware, (req, res) => {
+  if (req.user.user_type === "admin") {
+    try {
+      con.query(
+        `DELETE FROM albums  WHERE product_id="${req.params.id}"`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.send("Access Denied");
+  }
+});
+
+module.exports = router;
